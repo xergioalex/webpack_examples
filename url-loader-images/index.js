ea74f4b9231096996a94042182ea218e082768c8
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2018-08-04 23:43:38 -0500

    Using url-loader

---
+++ url-loader-images/index.js
@@ -0,0 +1,14 @@
+import './styles.css'
+import { firstMessage, delayedMessage } from './message.js'
+import platziImag from './xergioalex.jpg'
+
+document.write(firstMessage);
+delayedMessage()
+
+const img = document.createElement('img');
+img.setAttribute('src', platziImag)
+img.setAttribute('width', 50)
+img.setAttribute('height', 50)
+document.body.append(img);
+
+console.log("Hello world! from webpack");
---
+++ url-loader-images/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+MiniCssExtractPlugin.extract = function (param) {
+
+}
+
+
+module.exports = {
+	entry: path.resolve(__dirname, 'index.js'),
+	output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+	},
+  module: {
+    rules: [
+      // Rules for loaders
+      {
+        // test: What type i want to recognize
+        // use: What loader will be load
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        // test: What type i want to recognize
+        // use: What loader will be load
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 200000
+          }
+        }
+      },
+      // Rules for loaders
+      {
+        // test: What type i want to recognize
+        // use: What loader will be load
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+}
