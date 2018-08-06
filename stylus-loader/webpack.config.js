274432d8e5bc1e46a874d85269a5b32ed5d4e96f
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2018-08-06 18:24:50 -0500

    Using stylus loader

---
+++ stylus-loader/webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+MiniCssExtractPlugin.extract = function (param) {
+
+}
+
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src/js/index.js'),
+	output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+	},
+  module: {
+    rules: [
+      // Rules for loaders
+      {
+        // test: What type i want to recognize
+        // use: What loader will be load
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+      {
+        // test: What type i want to recognize
+        // use: What loader will be load
+        test: /\.(jpg|png|woff|eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 200000
+          }
+        }
+      },
+      // Rules for loaders
+      {
+        // test: What type i want to recognize
+        // use: What loader will be load
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      // Rules for loaders
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+          // {
+          //   loader: 'stylus-loader',
+          //   options: {
+          //     use: [
+          //       require('nib'),
+          //       require('rupture')
+          //     ],
+          //     import: [
+          //       '~nib/lib/nib/index.styl',
+          //       '~rupture/rupture/nib/index.styl'
+          //     ]
+          //   }
+          // }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "src/css/[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+}
